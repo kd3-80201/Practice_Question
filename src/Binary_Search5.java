bc52928244f7373868a78c1c3dfede4066dbaae5
Afnan Baig <mirzaafnanbaig98@gmail.com>
2022-10-08 21:54:08 +0530

    initial commit

---
+++ src/Binary_Search5.java
@@ -0,0 +1,32 @@
+public class Binary_Search5 {
+    // https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+
+    /* Find The Smallest Letter Greater Than Target You are given an array of characters letters that is sorted in non-decreasing order,
+     and a character target. There are at least two different characters in letters.
+     Return the smallest character in letters that is lexicographically greater than target.
+     If such a character does not exist, return the first character in letters.     */
+    public static void main(String[] args) {
+        char[] letters = {'c','d', 'f', 'j'};
+        char target = 'f';
+         //char[] letters = {'x', 'x', 'y' , 'y'};
+        //char target = 'z';
+        char jk = nextGreatestLetter(letters, target);
+        System.out.println(jk);
+    }
+
+    static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
+
